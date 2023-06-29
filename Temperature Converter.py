6ab873fe8a165d21a6e90bbe4b3287b6d71227cf
AznRunner <137722876+AznRunner@users.noreply.github.com>
2023-06-28 21:34:39 -0400

    Converter
    changes Celsius to degrees and vice versa

---
+++ Temperature Converter.py
@@ -0,0 +1,28 @@
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def main():
+    print("Temperature Converter")
+    print("1. Celsius to Fahrenheit")
+    print("2. Fahrenheit to Celsius")
+    choice = int(input("Enter your choice (1 or 2): "))
+
+    if choice == 1:
+        celsius = float(input("Enter temperature in Celsius: "))
+        fahrenheit = celsius_to_fahrenheit(celsius)
+        print("Temperature in Fahrenheit:", fahrenheit)
+    elif choice == 2:
+        fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+        celsius = fahrenheit_to_celsius(fahrenheit)
+        print("Temperature in Celsius:", celsius)
+    else:
+        print("Invalid choice!")
+
+if __name__ == '__main__':
+    main()
